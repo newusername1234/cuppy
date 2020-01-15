72de133bd97a168490302e3bc6beab237c59f0ab
Wes <wesleyayer92@gmail.com>
2020-01-15 09:54:26 -0500

    adds basic stuff

---
+++ models/connection.js
@@ -0,0 +1,13 @@
+const pgp = require('pg-promise')({
+    query: e => {
+        console.log(`QUERY: ${e.query}`);
+    }
+});
+
+const options = {
+    host: 'localhost',
+    database: 'coffeebase'
+};
+
+const db = pgp(options);
+module.exports = db;
