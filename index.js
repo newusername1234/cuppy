6e8c4a045fa31df2180fccb434cc8eacc9c58ae6
Wes <wesleyayer92@gmail.com>
2020-01-15 14:29:29 -0500

    adds html templates and routing for roasters, greencoffee

--- index.js
+++ index.js
@@ -32,9 +32,21 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/new/greencoffee', (req, res) => {
+    res.render('greencoffee');
+});
 
+app.post('/new/greencoffee', parseForm, (req, res) => {
+    
+});
 
+app.get('/new/roaster', (req, res) => {
+    res.render('roaster');
+});
 
+app.post('/new/roaster', parseForm, (req, res) => {
+
+});
 
 server.listen(PORT, () => {
     console.log(`server listening at ${PORT}`)
