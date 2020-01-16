8e14e21573786a87035e2bf84153530600d64290
Austin Dryden <austinisnorobot@gmail.com>
2020-01-16 12:09:15 -0500

    API working!!

--- routes/api.js
+++ routes/api.js
@@ -11,45 +11,52 @@ router.get('/', (req,res)=>{
 });
 
 //returns name and id for all shops
-router.get('/shops',async (req,res)=>{
-    const shops = await api.allShops;
+router.get('/shops', async (req,res)=>{
+    const shops = await api.allShops();
     res.json(shops);
 });
 
 //returns full info for given shop, and all roasters using/used
-router.get('/shops/:id', (req,res)=>{
-    res.json("");
+router.get('/shops/:id', async (req,res)=> {
+    const shop = await api.oneShop(req.params.id);
+    res.json(shop);
 });
 
 //returns name and id for all whole bean coffees
-router.get('/bean', (req,res)=>{
-    res.json("");
+router.get('/bean', async (req,res)=>{
+    const beans = await api.allBeans();
+    res.json(beans);
 });
 
 //returns full info for given whole bean coffee
-router.get('/bean/:id', (req,res)=>{
-    res.json("");
+router.get('/bean/:id', async (req,res)=>{
+    const bean = await api.oneBean(req.params.id);
+    res.json(bean);
 });
 
 
 //returns name and id for all roasters
-router.get('/roasters', (req,res)=>{
-    res.json("");
+router.get('/roasters', async (req,res)=>{
+    const roasters = await api.allRoasters();
+    res.json(roasters);
 });
 
 //returns full info for given roaster, including list of cafes using
-router.get('/roasters/:id', (req,res)=>{
-    res.json("");
+router.get('/roasters/:id', async (req,res)=>{
+    const roaster = await api.oneRoaster(req.params.id);
+    res.json(roaster);
 });
 
 //returns full info for all green coffees.
-router.get('/green', (req,res)=>{
-    res.json("");
+router.get('/green', async (req,res)=>{
+    const greens = await api.allGreen();
+    res.json(greens);
 });
 
 //returns full info for given green coffee id.
-router.get('/green/:id', (req,res)=>{
-    res.json("");
+router.get('/green/:id', async (req,res)=>{
+    const green = await api.oneGreen(req.params.id);
+    res.json(green);
 });
 
 
