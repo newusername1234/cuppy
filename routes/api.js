42ea63d4dafc29d84eed3fefda37b7d001562090
Austin Dryden <austinisnorobot@gmail.com>
2020-01-16 09:35:54 -0500

    edits to schema and seed.

--- routes/api.js
+++ routes/api.js
@@ -5,5 +5,16 @@ router.get('/testing', (req, res)=>{
     res.send('yeet');
 });
 
+router.get('/', (req,res)=>{
+    res.render('api');
+});
+
+router.get('/shops', (req,res)=>{
+    res.json("");
+});
+
+router.get('/')
+
+
 
 module.exports = router;
