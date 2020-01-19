e318d06d82306d313b0f6dec122fa0a7496ac195
Austin Dryden <austinisnorobot@gmail.com>
2020-01-19 16:52:25 -0500

    adds valid Hash and api keys to seed data.

--- seed.sql
+++ seed.sql
@@ -6,11 +6,11 @@ insert into greenCoffee (name, countryOfOrigin, regionOfOrigin, farm, farmer, el
 VALUES
 ('Finca La Gaju','Rwanda', 'Coko-Gaju', 'finca la Gaju' ,'Farmer Joe', 1800, 'bourbon', 'washed');
 
-insert into users (username, firstName, lastName, email, phoneNumber, hash)
+insert into users (username, firstName, lastName, email, phoneNumber, hash, apikey, apicalls, apitimestamp)
 VALUES
-('austin', 'Austin', 'Dryden', 'austinisnorobot@gmail.com', '404-888-9999', 'browns'),
-('pat', 'Patrick', 'Easterly', 'pattypatpat@gmail.com', '123-333-4567', 'browns'),
-('wes', 'Wes', 'Ayer', 'wesayer@gmail.com', '222-324-4532', 'browns');
+('austin', 'Austin', 'Dryden', 'austinisnorobot@gmail.com', '813-545-74347', '$2a$10$v7raUPPEt0dXSsPI3dDQiuZnKPHA6uHnMISJ7FjQqqJeYUJVznZpq', '292100f9-76cb-4a63-be7b-2ea67e901c09', 0 , '2020-01-19 10:10:10'),
+('naw', 'Patrick', 'Easterly', 'pattypatpat@gmail.com', '123-333-4567', '$2a$10$87T/mbwvdWDWvlSKCnFgeegGmCX2KS08fsvtZiBHMYH2ukg.fFe.G', '292100f9-76cb-4a63-be7b-2ea67e901c09', 0 , '2020-01-19 10:10:10'),
+('sew', 'Wes', 'Ayer', 'wesayer@gmail.com', '222-324-4532', '$2a$10$btl.cHk65GYxdmBRBcgs9u2T89yDOZoNau4rlUAGxWIwYTKZE6.0q', 'bf4feef2-17f8-404a-ad5c-65a602b4685b', 0 , '2020-01-19 10:10:10');
 
 insert into shops (name, location, phoneNumber, hours, website, shopOwnerID)
 VALUES
@@ -24,7 +24,7 @@ VALUES
 
 insert into cups (userID, name, dateOrdered, roastDate, cost, brewMethod, coffeeSize, condiments, didLike, flavor, aroma, acidity, sweetness, mouthfeel, comments, score, shopID, beanCoffeeID)
 VALUES
-(2, 'chai', '2020-01-02', '2020-01-07', '4.5', '', '12oz', 'Almond Milk, Splenda', 'y', 'spicy', 'farts', 'lemon', 'honey', 'milky', 'its a chai, it was fine', 5, 1, NULL),
+(1, 'chai', '2020-01-02', '2020-01-07', '4.5', '', '12oz', 'Almond Milk, Splenda', 'y', 'spicy', 'farts', 'lemon', 'honey', 'milky', 'its a chai, it was fine', 5, 1, NULL),
 (1, 'brewed coffee', '2019-12-15', '2020-01-07', '2.5', 'Chemex', '12oz', 'none', 'y', 'cocoa', 'cherry', 'blood orange', 'brown sugar', 'silky', 'a little underwhelming. too acidic', 3,1,1);
 
 insert into roasters_shops (roasterID, shopID)
